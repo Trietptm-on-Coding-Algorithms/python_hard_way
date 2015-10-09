189e4ef43f550a5e0d6070b2ddbdbdc9a878f462
Paweł Gawlas <pagawlas+github@gmail.com>
2015-10-10 01:23:28 +0200

    Commit

---
+++ Exercise 27 - Memorizing Logic.py
@@ -0,0 +1,16 @@
+print not True
+print not False
+
+print True or False
+print True or True
+print False or True
+print False or False
+
+print True and False
+print True and True
+print False and True
+print False and False
+
+print not(True or False)
+print not(True and True)
+print (not False) and True
