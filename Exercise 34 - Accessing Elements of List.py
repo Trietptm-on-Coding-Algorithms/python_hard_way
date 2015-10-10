d9164c94ff8bcd5e1b23cd989836bb1936a29fad
Paweł Gawlas <pagawlas+github@gmail.com>
2015-10-10 19:36:46 +0200

    Commit

---
+++ Exercise 34 - Accessing Elements of List.py
@@ -0,0 +1,18 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# the animal at 1
+print animals[1]
+# the third animal
+print animals[3-1]
+# the first animal
+print animals[1-1]
+# the animal at 3
+print animals[3]
+# the fifth animal
+print animals[4]
+# the animal at 2
+print animals[2]
+# the sixth animal
+print animals[5]
+# the animal at 4
+print animals[4]
